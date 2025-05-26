f14e47421ba40b4224103b8d282d8ffc50692d0a
Andrew <Andrew.Figure.09@gmail.com>
2025-05-26 18:32:54 -0400

    Main uses new scanner

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <string>
 
-#include "DynamicArray.h"
-#include "LinkedList.h"
+#include "containers/DynamicArray.h"
+#include "containers/LinkedList.h"
 #include "Scanner.h"
+#include "wrappers/Wrappers.h"
 
 int main() {
     // Fill and trim DynamicArray and LinkedList of ints.
@@ -40,35 +41,33 @@ int main() {
     }
     
     // Scan and print integer container contents.
+    auto wrapped_ints_arr = make_iterable_wrapper(ints_arr);
+    auto wrapped_ints_list = make_iterable_wrapper(ints_list);
+
     Print<int> int_printer{std::cout};
-    scan_and_apply(ints_arr, int_printer);
+    scan_and_apply(wrapped_ints_arr, int_printer);
     std::cout << std::endl;
-    scan_and_apply(ints_list, int_printer);
+    scan_and_apply(wrapped_ints_list, int_printer);
     std::cout << std::endl;
 
     // Scan and print string container contents.
+    auto wrapped_str_arr = make_iterable_wrapper(strings_arr);
+    auto wrapped_str_list = make_iterable_wrapper(strings_list);
+
     Print<std::string> str_printer{std::cout};
-    scan_and_apply(strings_arr, str_printer);
+    scan_and_apply(wrapped_str_arr, str_printer);
     std::cout << std::endl;
-    scan_and_apply(strings_list, str_printer);
+    scan_and_apply(wrapped_str_list, str_printer);
     std::cout << std::endl;
-
-    // Print averages for integer containers.
-    // std::cout << ints_arr.get_average() << std::endl;
-    // std::cout << ints_list.get_average() << std::endl;
-
-    // Attempting to get string averages is a compilation error.
-    // std::cout << strings_arr.get_average() << std::endl;
-    // std::cout << strings_list.get_average() << std::endl;
 
     // Compute and print aggregates using scan_and_apply
     Sum<int> int_total;
     Max<int> int_maximum;
     Min<int> int_minimum;
 
-    scan_and_apply(ints_list, int_total);
-    scan_and_apply(ints_list, int_maximum);
-    scan_and_apply(ints_list, int_minimum);
+    scan_and_apply(wrapped_ints_arr, int_total);
+    scan_and_apply(wrapped_ints_arr, int_maximum);
+    scan_and_apply(wrapped_ints_arr, int_minimum);
 
     std::cout << "Sum of ints: " << int_total.total << "\n";
     std::cout << "Max of ints: " << int_maximum.value << "\n";
@@ -77,21 +76,51 @@ int main() {
     Sum<std::string> str_total;
     Max<std::string> str_max;
     Min<std::string> str_min;
-    scan_and_apply(strings_list, str_total); // This will work fine using concatenation.
-    scan_and_apply(strings_list, str_max);
-    scan_and_apply(strings_list, str_min);
+
+    scan_and_apply(wrapped_str_list, str_total); // This will work fine using concatenation.
+    scan_and_apply(wrapped_str_list, str_max);
+    scan_and_apply(wrapped_str_list, str_min);
 
     std::cout << "Sum of strings: " << str_total.total << "\n";
     std::cout << "Max of strings: " << str_max.value << "\n";
     std::cout << "Min of strings: " << str_min.value << "\n";
 
     Average<int> int_avg;
-    scan_and_apply(ints_list, int_avg);
+    scan_and_apply(wrapped_ints_arr, int_avg);
     std::cout << "Int avg: " << int_avg.result() << "\n";
 
     // Average<std::string> str_avg;
-    // scan_and_apply(strings_list, str_avg);
+    // Average<std::string> str_avg;
+    // scan_and_apply(wrapped_str_list, str_avg);
     // std::cout << "Str Avg: " << str_avg.result() << "\n"; // This line won't compile!
+
+    LinkedList<float> floats_list;
+    for (int i = 1; i <= 10; ++i) {
+        floats_list.push(static_cast<float>(i * i / (i + 7.0)));
+    }
+
+    auto wrapped_float_arr = make_iterable_wrapper(floats_list);
+    
+    Print<float> float_printer{std::cout};
+    scan_and_apply(wrapped_float_arr, float_printer);
+    std::cout << std::endl;
+
+    Sum<float> float_total;
+    Max<float> float_maximum;
+    Min<float> float_minimum;
+
+    scan_and_apply(wrapped_float_arr, float_total);
+    scan_and_apply(wrapped_float_arr, float_maximum);
+    scan_and_apply(wrapped_float_arr, float_minimum);
+
+    std::cout << "Sum of floats: " << float_total.total << "\n";
+    std::cout << "Max of floats: " << float_maximum.value << "\n";
+    std::cout << "Min of floats: " << float_minimum.value << "\n";
+
+    Average<float> float_avg;
+    scan_and_apply(wrapped_float_arr, float_avg);
+    std::cout << "Float avg: " << float_avg.result() << "\n";
+
 
     return 0;
 }
